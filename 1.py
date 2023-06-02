5a204216c5b6c85dbfb8e4a600be67fc484f8df5
nima-mojarradi <135042990+nima-mojarradi@users.noreply.github.com>
2023-06-02 16:13:14 +0330

    Update 1.py

---
+++ 1.py
@@ -0,0 +1,13 @@
+import pickle
+class Person():
+    def __init__(self,
+                name,
+                age,
+                address)
+            self.name = name
+            self.age = age
+            self.address = address
+
+def pickling(obj_list):
+    picle_list = open('list.pickle', "wb")
+    pick.dump
